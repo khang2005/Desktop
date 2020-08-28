5cbbf427d534e02f1fc6a6532348b62fb656e49d
Admin <admin@admins-mbp.home>
2020-08-27 17:30:56 -0700

    firstcommit

---
+++ JSexcercise/01basics/celsuis.js
@@ -0,0 +1,11 @@
+var score = 100
+const bonus = 120
+let totalScore = (score - bonus) 
+console.log(totalScore)
+let tempInFahrenheit = 340
+// caculation
+//  (T(°F) - 32) × 5/9
+
+let celsius = (tempInFahrenheit - 32) * 5/9 
+
+console.log(celsius)
