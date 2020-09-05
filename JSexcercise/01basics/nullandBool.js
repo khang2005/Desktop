939368fd5da76e286a2c7d63645673402bb34d6b
Admin <admin@Admins-MacBook-Pro.local>
2020-09-05 14:11:27 -0700

    addvar

---
+++ JSexcercise/01basics/nullandBool.js
@@ -0,0 +1,25 @@
+// temp is coming from website API
+
+var temp = 0
+
+// done some calculation
+temp = 2 
+
+
+console.log("Current temperature is: " + temp)
+
+
+// Grade system
+
+// 10 - Great job
+// 5 - work harder
+// 2 - poor
+// 0 - fail
+
+// Boolean - True/false
+
+let actualMarks = 10
+
+let myGrade = (actualMarks > 10)
+
+console.log(myGrade)
