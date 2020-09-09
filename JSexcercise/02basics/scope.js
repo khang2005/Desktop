585fae4b8ca511315bd446385e06903819657ae4
Admin <admin@admins-mbp.home>
2020-09-08 21:07:24 -0700

    kingsProblem

--- JSexcercise/02basics/scope.js
+++ JSexcercise/02basics/scope.js
@@ -1,12 +1,30 @@
-let iAmGlobal = 'someValue'
+// let iAmGlobal = 'someValue'
 
-if (true){
-    let iamLocal = 'someMoreValue'
-    iAmGlobal = 'superman'
-    console.log(iAmGlobal);
-    console.log(iamLocal)
+// if (true){
+//     let iamLocal = 'someMoreValue'
+//     iAmGlobal = 'superman'
+//     console.log(iAmGlobal);
+//     console.log(iamLocal)
 
-} 
+// } 
 
-console.log(iamLocal);
-console.log(iAmGlobal);
+// console.log(iamLocal)
+// console.log(iAmGlobal);
+
+
+//Kings problem
+
+// let king = 'John'
+
+if (true) {
+    // let king = 'Sam'
+
+    if (true) {
+        let king = 'Ram'
+        console.log(king);
+    }
+}
+// if (true) {
+//     console.log(" I am second part : " + king);
+
+// }
