afbe9c6080c7be361679ae470b284dd6ac7507ba
Admin <admin@admins-mbp.home>
2020-09-11 18:22:28 -0700

    howtodelete

--- JSexcercise/02basics/marvel.js
+++ JSexcercise/02basics/marvel.js
@@ -1,10 +1,36 @@
-// datatype - Array
-// start with 0
-const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America', 'Thor'] 
+// // datatype - Array
+// // start with 0
+// const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America', 'Thor'] 
 
-console.log(superHeroes);
-console.log(superHeroes[0]);
-console.log(superHeroes[2]);
-console.log(superHeroes[superHeroes.length -1]);
+// console.log(superHeroes);
+// console.log(superHeroes[0]);
+// console.log(superHeroes[2]);
+// console.log(superHeroes[superHeroes.length -1]);
 
-console.log(`WE have ${superHeroes[superHeroes.length -1]} super heroes`);
+// console.log(`WE have ${superHeroes[superHeroes.length -1]} super heroes`);
+
+
+const numbers = ['One', 'Two','Three', 'Four','Five', 'Six']
+
+// numbers[1] = 'SOMETHING'
+// console.log(numbers);
+
+// start
+
+// console.log(numbers.shift());
+
+// console.log(numbers);
+// numbers.unshift('SOMETHING')
+// console.log(numbers);
+
+// end
+// console.log("The element to be deleted is:" + numbers.pop());
+// console.log(numbers);
+
+// numbers.push('Seven')
+// console.log(numbers);
+// Middle
+numbers.splice(5, 2, 'Khang Tran')
+console.log(numbers);
+
+
